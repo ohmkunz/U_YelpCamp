4ec04deeb3ba9c555719b848ac1b40a46b410b81
ohmkunz <ohmkunz@gmail.com>
2022-01-10 15:51:57 +0700

    can add new Campground

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ mongoose.connect('mongodb://localhost:27017/yelp-camp', { useNewUrlParser: true,
 const app = express()
 app.set('views engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
     res.render('home.ejs')
@@ -24,6 +25,24 @@ app.get('/campgrounds', async (req, res) => {
     const campgrounds = await Campground.find({})
     res.render('campgrounds/index.ejs', { campgrounds })
 })
+
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new.ejs')
+})
+
+app.post('/campgrounds', async (req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save()
+    res.redirect(`/campgrounds/${campground._id}`)
+})
+
+
+app.get('/campgrounds/:id', async (req, res) => {
+    const campground = await Campground.findById(req.params.id)
+    res.render('campgrounds/show.ejs', { campground })
+})
+
+
 
 app.listen(3000, () => {
     console.log("Serving on port 3000")
