daa193d09df31126a2d9ab38f96ea4ac9bf992d2
ohmkunz <ohmkunz@gmail.com>
2022-01-23 16:44:17 +0700

    image upload and display in carousel

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -3,11 +3,19 @@ const router = express.Router();
 const campgrounds = require('../controllers/campgrounds')
 const catchAsync = require('../utils/catchAsync')
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware')
+const multer = require('multer')
+const { storage } = require('../cloudinary')
+const upload = multer({ storage })
+
+
 
 router.route('/')
     .get(catchAsync(campgrounds.index))
-    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
-
+    .post(isLoggedIn, upload.array('image'), validateCampground, catchAsync(campgrounds.createCampground))
+// .post(upload.array('image'), (req, res) => {
+//     console.log(req.body, req.files)
+//     res.send("DONE")
+// })
 router.get('/new', isLoggedIn, campgrounds.renderNewForm)
 
 router.route('/:id')
